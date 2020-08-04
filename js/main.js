806d8d119bd790135ebe323a83109dd5c1daf154
olekrumian <58187457+olekrumian@users.noreply.github.com>
2020-08-04 15:39:29 +0300

    osnova

---
+++ js/main.js
@@ -0,0 +1,41 @@
+const button = document.querySelector(".switch");
+const element = document.querySelector(".main-element");
+const activeClass = "main-theme-active";
+
+
+window.addEventListener("load", (event) => {
+    localStorage.getItem("theme") && element.classList.add(activeClass);
+});
+
+button.addEventListener("click", (event) => {
+    element.classList.toggle(activeClass);
+    localStorage.getItem("theme") ?
+        localStorage.removeItem("theme", activeClass) :
+        localStorage.setItem("theme", activeClass);
+});
+
+function burgerMenu(selector) {
+    let menu = $(selector);
+    let button = menu.find(".burger__menu-button");
+    let link = menu.find(".burger__menu-link");
+
+    button.on("click", (e) => {
+        e.preventDefault();
+        toggleMenu();
+    });
+
+    link.on("click", () => toggleMenu());
+    overlay.on("click", () => toggleMenu());
+
+    function toggleMenu() {
+        menu.toggleClass("burger__menu-active");
+
+        if (menu.hasClass("burger__menu-active")) {
+            $("body").css("overflow", "hidden");
+        } else {
+            $("body").css("overflow", "visible");
+        }
+    }
+}
+
+burgerMenu(".burger__menu");
