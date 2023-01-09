82e36753a42a274250b98bfd46df14e610bca2fd
Oleksandr Rumiantsev <rumyan87@gmail.com>
2023-01-09 13:14:06 +0100

    translate

--- js/main.js
+++ js/main.js
@@ -1,41 +1,39 @@
-const button = document.querySelector(".switch");
-const element = document.querySelector(".main-element");
-const activeClass = "main-theme-active";
+const button = document.querySelector('.switch')
+const element = document.querySelector('.main-element')
+const activeClass = 'main-theme-active'
 
+window.addEventListener('load', () => {
+  localStorage.getItem('theme') && element.classList.add(activeClass)
+})
 
-window.addEventListener("load", (event) => {
-    localStorage.getItem("theme") && element.classList.add(activeClass);
-});
-
-button.addEventListener("click", (event) => {
-    element.classList.toggle(activeClass);
-    localStorage.getItem("theme") ?
-        localStorage.removeItem("theme", activeClass) :
-        localStorage.setItem("theme", activeClass);
-});
+button.addEventListener('click', () => {
+  element.classList.toggle(activeClass)
+  localStorage.getItem('theme')
+    ? localStorage.removeItem('theme', activeClass)
+    : localStorage.setItem('theme', activeClass)
+})
 
 function burgerMenu(selector) {
-    let menu = $(selector);
-    let button = menu.find(".burger__menu-button");
-    let link = menu.find(".burger__menu-link");
+  let menu = $(selector)
+  let button = menu.find('.burger__menu-button')
+  let link = menu.find('.burger__menu-link')
 
-    button.on("click", (e) => {
-        e.preventDefault();
-        toggleMenu();
-    });
+  button.on('click', (e) => {
+    e.preventDefault()
+    toggleMenu()
+  })
 
-    link.on("click", () => toggleMenu());
-    overlay.on("click", () => toggleMenu());
+  link.on('click', () => toggleMenu())
 
-    function toggleMenu() {
-        menu.toggleClass("burger__menu-active");
+  function toggleMenu() {
+    menu.toggleClass('burger__menu-active')
 
-        if (menu.hasClass("burger__menu-active")) {
-            $("body").css("overflow", "hidden");
-        } else {
-            $("body").css("overflow", "visible");
-        }
+    if (menu.hasClass('burger__menu-active')) {
+      $('body').css('overflow', 'hidden')
+    } else {
+      $('body').css('overflow', 'visible')
     }
+  }
 }
 
-burgerMenu(".burger__menu");
+burgerMenu('.burger__menu')
